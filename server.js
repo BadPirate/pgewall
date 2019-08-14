e762ad8347e75acc3c4c9c50fa752a2165321d00
Kevin Lohman <badpirate@fb.com>
2019-08-14 13:39:45 -0700

    server logging, heroku debug

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ app.get('/api/enlighten/:user_id/:system/production/:start/:end', (req, res) => 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
+  console.log('Non-API Request - ',req);
   res.sendFile(path.join(__dirname+'/build/index.html'));
 });
 
