10090adac797cf8421bd2f015cdd0a89ad995fc3
Kevin Lohman <badpirate@fb.com>
2019-08-14 12:50:54 -0700

    add privacy info about tracking if present

--- src/App.js
+++ src/App.js
@@ -23,6 +23,11 @@ class App extends React.Component
               </h5>
               <p>This site is Javascript (runs locally) so no data is sent to our servers or uploaded.  I don't want it.
               But don't take my word for it, <a href="https://github.com/BadPirate/pgewall">download and checkout the code yourself</a>.</p>
+              {
+                process.env.REACT_APP_GOOGLE_TRACKING_ID
+                ? <p>Google Tracking (page visits, and logging) has been enabled so that I can see a rough count of people using (or failing to use) the tool successfully.</p>
+                : null
+              }
             </Alert>
             <Alert variant="warning">
               <h5>
